cc7f1b141fb112e05b8e2128da6f0cff01a1998a
Sushil Kudav <sushil.kudav@dsmindia.com>
2013-07-01 17:41:42 +0530

    First Time Project Creation
    Contains the vertical Menu and the Screens

---
+++ ServiceDeskApp/ServiceDeskApp/Controllers/ServiceDeskAppController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDeskApp.Controllers
+{
+    public class ServiceDeskAppController : Controller
+    {
+        //
+        // GET: /ServiceDeskApp/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult address()
+        {
+            return View();
+        }
+        public ActionResult organization()
+        {
+            return View();
+        }
+        public ActionResult person()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ServiceDeskApp/ServiceDeskApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceDeskApp.MvcApplication" Language="C#" %>
